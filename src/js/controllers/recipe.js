8ce0aacd688a96818ca86ef9f7dfb2e0a5964ac7
382229841 <382229841@qq.com>
2015-08-12 14:54:50 +0800

    --

--- src/js/controllers/recipe.js
+++ src/js/controllers/recipe.js
@@ -4,6 +4,54 @@ app.controller('recipeController', function($scope, $rootScope, $interval) {
 	$rootScope.head =$rootScope.dish? $rootScope.dish.name : 'Create Recipe Page';
 	
 	
+	//var arrButton = ["取消","确定"];
+	//var msg="";
+	$scope.setHeater=function(){
+		openDialog('html',"heater.html", null, 3,
+			function (r) {
+				if (r) {
+					
+				}
+		});
+	};
+	
+	$scope.setBox=function(){
+		openDialog('html',"box.html", null, 3,
+			function (r) {
+				if (r) {
+					
+				}
+		});
+	};
+	
+	$scope.setStirrer=function(){
+		openDialog('html',"stirrer.html", null, 3,
+			function (r) {
+				if (r) {
+					
+				}
+		});
+	};
+	
+	$scope.setRepeater=function(){
+		openDialog('html',"repeater.html", null, 3,
+			function (r) {
+				if (r) {
+					
+				}
+		});
+	};
+	
+	$scope.setTimer=function(){
+		openDialog('html',"timer.html", null, 3,
+			function (r) {
+				if (r) {
+					
+				}
+		});
+	};
+	
+	
 
 	
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-var serviceUrl="http://test.yhiker.com/api";
+var serviceUrl="";
 var statusCode = {
     Success: 0,
     error: -1
@@ -39,25 +39,8 @@ function ajaxRequest(api,requestData,callback){
         }
     });
 }
-function FAQTemplate(items){
-	var html="";
-	for(var i=0;i<items.length;i++){
-		html=html+
-		"<div class=\"question\">"+
-				"<span class=\"qfa-title\">Q：</span>"+
-				"<span class=\"qfa-content\">"+items[i].question+"</span>"+
-			"</div>"+
 
-			"<div class=\"answer\">"+
-				"<span class=\"qfa-title\">A：</span>"+
-				"<span class=\"qfa-content\">"+items[i].answer+"</span>"+
 
-			"</div>";
-		
-
-	}
-	return html;
-}
 function initApp(){
 	$(".touchable").on("touchstart MSPointerDown pointerdown",function(){
 		$(this).addClass('touched');
@@ -73,10 +56,341 @@ function initApp(){
 		$(this).removeClass('touched');
 	});
 	
-	$(".dropdown").on("touch click",function(){		
+	$(".dropdown .dropdown-btn").on("touch click",function(){		
 		$(".dropdown .dropdown-items").hasClass("open")?
 		$(".dropdown .dropdown-items").removeClass("open")
 		:
 		$(".dropdown .dropdown-items").addClass("open");
 	});
+	
+	$(".dropdown .dropdown-items").on("touch click",function(){	
+		$(".dropdown .dropdown-items").removeClass("open");
+	});
 }
+
+
+
+
+
+
+
+
+
+
+var dialogTimeOutId = null;
+var zoom = 1;
+var DialogType = { Default: 0, Image: 1, Success: 2, Prompt: 3,Pay:4,Load:5 };
+function openDialog(content, title, buttonArray, type, fn) {
+    zoom = 1;
+    var dialog = $("<div class='dialog'></div>");
+    var mask = $("<div class='mask'></div>");
+    var close = $('<img style="display:none;" class="btn-close touchable" src="images/close.png" />');
+    mask.appendTo("body");
+    dialog.appendTo("body");
+    close.appendTo("body");
+    close.unbind("click").bind("click", function () {
+        closeDialog();
+    });
+    if (content) {
+        if (type == DialogType.Image) {
+            mask.addClass("deeper");
+            var img = $('<div class="img-large"><img src="' + content + '" /></div>');
+            img.appendTo(dialog);
+            mask.unbind().bind("click", function () {
+                closeDialog();
+            });
+
+            img.unbind().bind("pinchopen", function () {
+                zoom++;
+                zoomImage(mask, img);
+            });
+
+            img.bind("pinchclose", function () {
+                zoom--;
+                zoomImage(mask, img);
+            });
+
+            img.bind("click", function () {
+                if (zoom == 3) {
+                    closeDialog();
+                }
+                else {
+                    zoom = 3;
+                    zoomImage(mask, img);
+                }
+            });
+        }
+        else if (type == DialogType.Success) {
+            var dialogWrap = $("<div class='dialog-success'></div>");
+            dialogWrap.appendTo(dialog);
+            dialogWrap.append($('<div class="cart-success"></div>'));
+            dialogWrap.append($('<span>' + content + '</span>'));
+        }
+        else if(type == DialogType.Prompt){
+            var dialogWrap = $("<div class='dialog-body'></div>");
+            dialogWrap.appendTo(dialog);
+
+            var matchWord = /html/i;
+            if (matchWord.test(arguments[0])) {
+                $(".dialog").addClass("html_text");
+                $(".dialog").load('views/' + title,function(){
+					$(".btn-close").show();
+					if ($(".dialog .btn").length > 0) {
+						$(".dialog .btn").click(function () {                    
+							var flag = $(this).attr("promo").replace("button", "");
+							if (fn) {
+								var inputs=$(".dialog .dialog-body p input");
+								var values=[];
+								for(var i=0;i<inputs.length;i++){
+									var v=$(inputs[i]).val();
+									values.push(v);
+								}
+								
+								fn(flag == '1',values);
+							}
+							$(".btn-close").remove();
+							$(".mask,.dialog").remove();
+						});
+					}
+					
+				});
+
+            } else {
+                if (title && title != '') {
+                    var dialogName = $("<h5></h5>");
+                    dialogName.appendTo(dialogWrap);
+                    dialogName.text(title);
+                }
+                var dialogText = $("<p></p>");
+                dialogText.appendTo(dialogWrap);
+                dialogText.html(content);
+
+                if (buttonArray && buttonArray.length > 0) {
+                    dialogWrap.addClass("dialog-confirm");
+                    var dialogFooter = $("<div class='dialog_footer justified'></div>");
+                    dialogFooter.appendTo(dialogWrap);
+                    for (var i = 0; i < buttonArray.length; i++) {
+                        var dialogButton = $("<div class='btn' promo='button" + i + "'></div>");
+                        dialogButton.append(buttonArray[i]);
+                        dialogButton.appendTo(dialogFooter);
+                    }
+                }
+            }
+            if ($(".dialog .btn").length > 0) {
+                $(".dialog .btn").click(function () {                    
+                    var flag = $(this).attr("promo").replace("button", "");
+                    if (fn) {
+                        var inputs=$(".dialog .dialog-body p input");
+                        var values=[];
+                        for(var i=0;i<inputs.length;i++){
+                            var v=$(inputs[i]).val();
+                            values.push(v);
+                        }
+                        
+                        fn(flag == '1',values);
+                    }
+					$(".btn-close").remove();
+                    $(".mask,.dialog").remove();
+                });
+            }
+            
+        }
+        else if(type == DialogType.Pay){
+            var dialogWrap = $("<div class='dialog-body'></div>");
+            var payWay=easybuy.PaymentMethods.AlipayWeb;
+            dialogWrap.appendTo(dialog);
+
+            if (title && title != '') {
+                var dialogName = $("<h5></h5>");
+                dialogName.appendTo(dialogWrap);
+                dialogName.text(title);
+            }
+
+            dialogWrap.append($('#pay-area').html());
+            if (buttonArray && buttonArray.length > 0) {
+                dialogWrap.addClass("dialog-confirm");
+                var dialogFooter = $("<div class='dialog_footer justified'></div>");
+                dialogFooter.appendTo(dialogWrap);
+                for (var i = 0; i < buttonArray.length; i++) {
+                    var dialogButton = $("<div class='btn' promo='button" + i + "'></div>");
+                    dialogButton.append(buttonArray[i]);
+                    dialogButton.appendTo(dialogFooter);
+                }
+            }
+
+            $(".dialog-body .list-group .list-group-item .check-area .radio").bind("click",function(){
+                $(".dialog-body .list-group .list-group-item .check-area .radio").removeClass("checked");
+                $(this).addClass("checked");
+                payWay=parseInt($(this).attr("value"));
+            });
+
+            if ($(".dialog .btn").length > 0) {
+                $(".dialog .btn").click(function () {
+                    $(".mask,.dialog").remove();
+                    var flag = $(this).attr("promo").replace("button", "");
+                    if (fn) {
+                        fn(flag == '1',payWay);
+                    }
+                });
+            }
+        }
+        else if(type == DialogType.Load){
+            var dialogWrap = $("<div class='dialog-body'></div>");
+            dialogWrap.appendTo(dialog);
+
+            var matchWord = /html/i;
+            if (arguments.length == 1 && matchWord.test(arguments[0])) {
+                $(".dialog").addClass("html_text");
+                $(".dialog").load('views/' + title);
+
+            } else {
+                if (title && title != '') {
+                    var dialogName = $("<h5></h5>");
+                    dialogName.appendTo(dialogWrap);
+                    dialogName.text(title);
+                }
+                var dialogText = $("<p style='height:3em'></p>");
+                dialogText.appendTo(dialogWrap);
+                //dialogText.text(content);
+                dialogText.html("<i class=\"fa fa-spinner fa-spin loading-spinner\"></i>");                
+
+                if (buttonArray && buttonArray.length > 0) {
+                    dialogWrap.addClass("dialog-confirm");
+                    var dialogFooter = $("<div class='dialog_footer justified'></div>");
+                    dialogFooter.appendTo(dialogWrap);
+                    for (var i = 0; i < buttonArray.length; i++) {
+                        var dialogButton = $("<div class='btn' promo='button" + i + "'></div>");
+                        dialogButton.append(buttonArray[i]);
+                        dialogButton.appendTo(dialogFooter);
+                    }
+                }
+            }
+            if ($(".dialog .btn").length > 0) {
+                $(".dialog .btn").click(function () {
+                    $(".mask,.dialog").remove();
+                    var flag = $(this).attr("promo").replace("button", "");
+                    if (fn) {
+                        fn(flag == '1');
+                    }
+                });
+            }
+        }
+        else {
+            var dialogWrap = $("<div class='dialog-body'></div>");
+            dialogWrap.appendTo(dialog);
+
+            var matchWord = /html/i;
+            if (arguments.length == 1 && matchWord.test(arguments[0])) {
+                $(".dialog").addClass("html_text");
+                $(".dialog").load('views/' + title);
+
+            } else {
+                if (title && title != '') {
+                    var dialogName = $("<h5></h5>");
+                    dialogName.appendTo(dialogWrap);
+                    dialogName.text(title);
+                }
+                var dialogText = $("<p></p>");
+                dialogText.appendTo(dialogWrap);
+                dialogText.text(content);
+
+                if (buttonArray && buttonArray.length > 0) {
+                    dialogWrap.addClass("dialog-confirm");
+                    var dialogFooter = $("<div class='dialog_footer justified'></div>");
+                    dialogFooter.appendTo(dialogWrap);
+                    for (var i = 0; i < buttonArray.length; i++) {
+                        var dialogButton = $("<div class='btn' promo='button" + i + "'></div>");
+                        dialogButton.append(buttonArray[i]);
+                        dialogButton.appendTo(dialogFooter);
+                    }
+                }
+            }
+            if ($(".dialog .btn").length > 0) {
+                $(".dialog .btn").click(function () {
+                    $(".mask,.dialog").remove();
+                    var flag = $(this).attr("promo").replace("button", "");
+                    if (fn) {
+                        fn(flag == '1');
+                    }
+                });
+            }
+        }
+    }
+}
+
+function zoomImage(mask, img) {
+    if (zoom < 1)
+        zoom = 1;
+
+    if (zoom > 5)
+        zoom = 5;
+
+    if (zoom > 1) {
+        img.addClass("larger");
+    }
+    else {
+        img.removeClass("larger");
+    }
+
+    mask.css("width", zoom * 100 + "%");
+    img.css("width", zoom * 100 + "%");
+}
+
+function alertWarning(content) {
+    openDialog(content);
+    dialogTimeOutId = setTimeout('closeDialog()', 2000);
+}
+function alertExt(content,callback) {
+    openDialog(content);
+    /*dialogTimeOutId = setTimeout(function(){
+        if(callback){
+            closeDialog();
+            callback();
+        }
+    }, 2000);*/
+
+    var d = setTimeout(function(){
+        if(callback){
+            callback();
+            clearTimeout(d);
+        }
+    }, 1500);
+}
+
+function alertLoad() {
+    openDialog(true, null, null, DialogType.Load);
+}
+
+function alertSuccess(content) {
+    openDialog(content, null, null, DialogType.Success);
+    dialogTimeOutId = setTimeout('closeDialog()', 1500);
+}
+
+function openPrompt(content,title,btnArr,fn){
+    openDialog(content, title, btnArr, DialogType.Prompt,fn);
+}
+
+function closeDialog() {
+    $(".mask,.dialog,.btn-close").remove();
+
+    if (dialogTimeOutId) {
+        clearTimeout(dialogTimeOutId);
+    }
+    dialogTimeOutId = null;
+}
+
+function showLargeImage(imgUrl) {
+    openDialog(imgUrl, null, null, DialogType.Image);
+}
+
+function showImageLarger(_this) {
+    openDialog(_this.src, null, null, DialogType.Image);
+}
+
+window.showLoading = function () {
+    alertLoad();
+};
+
+window.hideLoading = function () {
+    closeDialog();
+};
